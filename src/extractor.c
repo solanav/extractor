021d932af7e9496826acf4bcb62c5de7e25a1090
solanav <solanav@qq.com>
2020-10-29 15:27:04 +0100

    Libextractor working perfectly (no cleaning memory yet)

--- src/extractor.c
+++ src/extractor.c
@@ -14,6 +14,11 @@ typedef struct {
   size_t data_len;
 } raw_data;
 
+typedef struct {
+  raw_data **arr;
+  size_t len;  
+} raw_data_array;
+
 _EXTRACTOR_EXTERN int
 json_output (
   void *handle,
@@ -24,30 +29,38 @@ json_output (
   const char *data,
 	size_t data_len
 ) {
-  raw_data *json_data = (raw_data *) handle;
+  /* Get more memory to save the new struct */
+  raw_data_array *rdata_array = (raw_data_array *) handle;
+  rdata_array->len++;
+  rdata_array->arr = realloc(rdata_array->arr, rdata_array->len * sizeof(raw_data));
+  rdata_array->arr[rdata_array->len - 1] = calloc(1, sizeof(raw_data));
+
+  /* Rename for easy access */
+  raw_data *rdata = rdata_array->arr[rdata_array->len - 1];
 
   /* Copy all data to our struct */
-  json_data->plugin_name = calloc(strlen(plugin_name) + 1, sizeof(char));
-  strcpy(json_data->plugin_name, plugin_name);
+  rdata->plugin_name = calloc(strlen(plugin_name) + 1, sizeof(char));
+  strcpy(rdata->plugin_name, plugin_name);
   
   const char *type_str = EXTRACTOR_metatype_to_string(type);
-  json_data->type = calloc(strlen(type_str) + 1, sizeof(char));
-  strcpy(json_data->type, type_str);
+  rdata->type = calloc(strlen(type_str) + 1, sizeof(char));
+  strcpy(rdata->type, type_str);
   
-  json_data->format = format;
+  rdata->format = format;
 
-  json_data->mime_type = calloc(strlen(data_mime_type) + 1, sizeof(char));
-  strcpy(json_data->mime_type, data_mime_type);
+  rdata->mime_type = calloc(strlen(data_mime_type) + 1, sizeof(char));
+  strcpy(rdata->mime_type, data_mime_type);
 
-  json_data->data = calloc(data_len, sizeof(char));
-  memcpy(json_data->data, data, data_len);
-  json_data->data_len = data_len;
+  rdata->data = calloc(data_len, sizeof(char));
+  memcpy(rdata->data, data, data_len);
+  rdata->data_len = data_len;
 
   return 0;
 }
 
 static ERL_NIF_TERM
 extract(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  int i;
   ErlNifBinary file_bin;
   struct EXTRACTOR_PluginList *plugins;
 
@@ -60,23 +73,29 @@ extract(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   }
 
   /* Extract the data */
-  raw_data *json_data = calloc(1, sizeof(raw_data));
-  EXTRACTOR_extract(plugins, NULL, file_bin.data, file_bin.size, &json_output, (void *) json_data);
+  raw_data_array *data = calloc(1, sizeof(raw_data_array));
+  data->arr = NULL;
+  data->len = 0;
+  EXTRACTOR_extract(plugins, NULL, file_bin.data, file_bin.size, &json_output, (void *) data);
 
   /* Remove the plugins */
   EXTRACTOR_plugin_remove_all(plugins);
 
-  /* Create list of data */
-  ERL_NIF_TERM ret = enif_make_tuple5(
-    env,
-    enif_make_string(env, json_data->plugin_name, ERL_NIF_LATIN1),
-    enif_make_string(env, json_data->type, ERL_NIF_LATIN1),
-    enif_make_int(env, json_data->format),
-    enif_make_string(env, json_data->mime_type, ERL_NIF_LATIN1),
-    enif_make_string(env, json_data->data, ERL_NIF_LATIN1)
-  );
+  /* Create list of data */\
+  ERL_NIF_TERM *list = calloc(data->len, sizeof(ERL_NIF_TERM));
+  for (i = 0; i < data->len; i++)
+  {
+    list[i] = enif_make_tuple5(
+      env,
+      enif_make_string(env, (data->arr)[i]->plugin_name, ERL_NIF_LATIN1),
+      enif_make_string(env, (data->arr)[i]->type, ERL_NIF_LATIN1),
+      enif_make_int(env, (data->arr)[i]->format),
+      enif_make_string(env, (data->arr)[i]->mime_type, ERL_NIF_LATIN1),
+      enif_make_string(env, (data->arr)[i]->data, ERL_NIF_LATIN1)
+    );
+  }
 
-  return ret;
+  return enif_make_list_from_array(env, list, data->len);
 }
 
 static ErlNifFunc funcs[] = {
