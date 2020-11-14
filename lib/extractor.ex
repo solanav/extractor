c4bae0f9840a44209d3baaef23fe7d72b916f890
solanav <solanav@qq.com>
2020-11-14 18:01:35 +0100

    Experimenting with advice from elixir forums

--- lib/extractor.ex
+++ lib/extractor.ex
@@ -7,6 +7,14 @@ defmodule Extractor do
 
   app = Mix.Project.config[:app]
 
+  def test do
+    {:ok, file_data} = File.read("/home/solanav/krptkn/final_form.png")
+    Extractor.extract(file_data)
+    :msacc.start(2_000)
+    :msacc.print()
+    :msacc.stop
+  end
+
   def init do
     path = :filename.join(:code.priv_dir(unquote(app)), 'extractor')
     :ok = :erlang.load_nif(path, 0)
--- src/extractor.c
+++ src/extractor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "erl_nif.h"
 #include "extractor.h"
@@ -20,7 +21,7 @@ typedef struct {
 } raw_data_array;
 
 _EXTRACTOR_EXTERN int
-json_output (
+text_output (
   void *handle,
   const char *plugin_name,
   enum EXTRACTOR_MetaType type,
@@ -31,18 +32,21 @@ json_output (
 ) {
   /* Get more memory to save the new struct */
   raw_data_array *rdata_array = (raw_data_array *) handle;
+  raw_data *rdata;
+  const char *type_str;
+
   rdata_array->len++;
   rdata_array->arr = realloc(rdata_array->arr, rdata_array->len * sizeof(raw_data));
   rdata_array->arr[rdata_array->len - 1] = calloc(1, sizeof(raw_data));
 
   /* Rename for easy access */
-  raw_data *rdata = rdata_array->arr[rdata_array->len - 1];
+  rdata = rdata_array->arr[rdata_array->len - 1];
 
   /* Copy all data to our struct */
   rdata->plugin_name = calloc(strlen(plugin_name) + 1, sizeof(char));
   strcpy(rdata->plugin_name, plugin_name);
   
-  const char *type_str = EXTRACTOR_metatype_to_string(type);
+  type_str = EXTRACTOR_metatype_to_string(type);
   rdata->type = calloc(strlen(type_str) + 1, sizeof(char));
   strcpy(rdata->type, type_str);
   
@@ -60,29 +64,55 @@ json_output (
 
 static ERL_NIF_TERM
 extract(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-  int i;
+  size_t i;
   ErlNifBinary file_bin;
+  raw_data_array *data;
+  ERL_NIF_TERM *list;
   struct EXTRACTOR_PluginList *plugins;
+  
+  clock_t t;
+  double time_taken;
 
   /* Extract the plugins */
-  plugins = EXTRACTOR_plugin_add_defaults(EXTRACTOR_OPTION_DEFAULT_POLICY);
+  printf("Extracting plugins...");
+  t = clock();
+
+  plugins = (struct EXTRACTOR_PluginList *) enif_priv_data(env);
+
+  t = clock() - t;
+  time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("[OK] [%f]\n\r", time_taken);
 
   /* Read file binary */
+  printf("Reading file binary...");
+  t = clock();
+
   if (enif_inspect_binary(env, argv[0], &file_bin) == 0) {
     return enif_make_badarg(env);
   }
 
+  t = clock() - t;
+  time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("[OK] [%f]\n\r", time_taken);
+
   /* Extract the data */
-  raw_data_array *data = calloc(1, sizeof(raw_data_array));
+  printf("Extracting the data...");
+  t = clock();
+
+  data = calloc(1, sizeof(raw_data_array));
   data->arr = NULL;
   data->len = 0;
-  EXTRACTOR_extract(plugins, NULL, file_bin.data, file_bin.size, &json_output, (void *) data);
+  EXTRACTOR_extract(plugins, NULL, file_bin.data, file_bin.size, &text_output, (void *) data);
 
-  /* Remove the plugins */
-  EXTRACTOR_plugin_remove_all(plugins);
+  t = clock() - t;
+  time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("[OK] [%f]\n\r", time_taken);
 
-  /* Create list of data */\
-  ERL_NIF_TERM *list = calloc(data->len, sizeof(ERL_NIF_TERM));
+  /* Create list of data */
+  printf("Creating list of data...");
+  t = clock();
+
+  list = calloc(data->len, sizeof(ERL_NIF_TERM));
   for (i = 0; i < data->len; i++)
   {
     list[i] = enif_make_tuple5(
@@ -95,7 +125,14 @@ extract(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
     );
   }
 
-  /* Destroy the data array */
+  t = clock() - t;
+  time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("[OK] [%f]\n\r", time_taken);
+
+  /* Destroy the data array
+  printf("Destroying the data array...");
+  t = clock();
+
   for (i = 0; i < data->len; i++)
   {
     free((data->arr)[i]->plugin_name);
@@ -108,31 +145,43 @@ extract(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   free(data->arr);
   free(data);
 
+  t = clock() - t;
+  time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("[OK] [%f]\n\r", time_taken);
+  */
+
   return enif_make_list_from_array(env, list, data->len);
 }
 
 static ErlNifFunc funcs[] = {
-  { "extract", 2, extract }
+  { "extract", 2, extract, ERL_NIF_DIRTY_JOB_CPU_BOUND }
 };
 
 static int
 load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info) {
+  printf("LOADING\n\r");
+  *priv = (void *) EXTRACTOR_plugin_add_defaults(EXTRACTOR_OPTION_DEFAULT_POLICY);
   return 0;
 }
 
 static int
 reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM info) {
+  printf("RELOADING\n\r");
+  *priv = (void *) EXTRACTOR_plugin_add_defaults(EXTRACTOR_OPTION_DEFAULT_POLICY);
   return 0;
 }
 
 static int
 upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM info) {
+  printf("UPGRADING\n\r");
+  *priv = (void *) EXTRACTOR_plugin_add_defaults(EXTRACTOR_OPTION_DEFAULT_POLICY);
   return load(env, priv, info);
 }
 
 static void
 unload(ErlNifEnv* env, void* priv) {
-  enif_free(priv);
+  printf("UNLOADING\n\r");
+  EXTRACTOR_plugin_remove_all((struct EXTRACTOR_PluginList *) priv);
 }
 
 ERL_NIF_INIT(Elixir.Extractor, funcs, &load, &reload, &upgrade, &unload)
